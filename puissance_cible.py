be1e22a6bc200be891434010d173dcb498010065
Adrien <adrien.lavoie4@gmail.com>
2026-07-12 15:57:47 -0400

    ajout de puissance_cible et legere modif GUI

---
+++ puissance_cible.py
@@ -0,0 +1,6 @@
+#Estimer puissance FTP du cycliste
+#Si pas possible, demander un input a l'utilisateur ou hardcode une valeur
+#Determiner les zones de Coggan
+#puissance cible est dans une zone de Coggan predeterminee
+#comparer puissance estimee avec puissance cible
+#par exemple, on veut garder le cycliste en zone 3 pour tout son entrainement
