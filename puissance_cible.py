5ab6b74c0c500a64ddecc6f5b29c151038d27c3a
Adrien <adrien.lavoie4@gmail.com>
2026-07-18 17:53:40 -0400

    puissance cible debut

--- puissance_cible.py
+++ puissance_cible.py
@@ -1,6 +1,11 @@
-#Estimer puissance FTP du cycliste
-#Si pas possible, demander un input a l'utilisateur ou hardcode une valeur
-#Determiner les zones de Coggan
-#puissance cible est dans une zone de Coggan predeterminee
-#comparer puissance estimee avec puissance cible
-#par exemple, on veut garder le cycliste en zone 3 pour tout son entrainement
+import capteur_imu
+
+#Donnees du test de puissance critique a entrer
+#FTP
+#puissance max pour effort 3 min
+#puissance max pour effort 10 min
+#puissance max pour effort 18 min
+
+
+
+
