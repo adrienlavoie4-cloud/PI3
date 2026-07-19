24ff61921bf190dc906dd2d4797f6abf26a5f923
Adrien <adrien.lavoie4@gmail.com>
2026-07-18 20:15:35 -0400

    puissance_cible suite

--- puissance_cible.py
+++ puissance_cible.py
@@ -1,3 +1,4 @@
+import math
 import capteur_imu
 
 #Donnees du test de puissance critique a entrer
@@ -6,6 +7,30 @@ import capteur_imu
 #puissance max pour effort 10 min
 #puissance max pour effort 18 min
 
+(yaw, pitch, roll)=capteur_imu.get_orientation()
+#pente_extreme>10
+#pente_abrupte>5
+#pente_moyenne>2
+#pente_descendante<0
+
+FTP=220
+
+puissance_cible=0
+
+if pitch_deg > 10:
+    facteur = 1.15   # pente extrême : gros effort, vitesse faible, peu d'aéro
+elif pitch_deg > 5:
+    facteur = 1.10   # pente abrupte
+elif pitch_deg > 2:
+    facteur = 1.05   # pente moyenne
+elif pitch_deg > 0:
+    facteur = 1.00   # faux plat montant
+elif pitch_deg > -3:
+    facteur = 0.85   # léger faux plat descendant : profiter de la gravité
+else:
+    facteur = 0.0     # descente marquée : rouler en roue libre, pas d'effort
+
+puissance_cible = facteur * FTP
 
 
 
